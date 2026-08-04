8b8d627e9f70eabfe2061ae343b0602c3cf9a766
ariq <ghazi.ariq18@gmail.com>
2026-08-04 13:55:48 +0700

    nambahin if

--- Dowhile/dowhile3.js
+++ Dowhile/dowhile3.js
@@ -3,21 +3,21 @@ let password;
 let valid = false;
 // Tulis kode do-while loop di sini untuk validasi password
 do {
-    password = prompt("Buat Password Anda: ");
-    let panjangCukup = password.length >= 6;
+    password = prompt("Buat Password Anda: "); //masukkan password
+    let panjangCukup = password.length >= 6;   //kondisi password minimal 6 karakter
 
-    let adaAngka = false;
-    for (let i = 0; i < password.length; i++) {
-        if (password[i] >= "0" && password[i] <= "9")
-            adaAngka = true;
+    let adaAngka = false; //kondisi awal false agar loop 
+    for (let i = 0; i < password.length; i++) {  
+        if (password[i] >= "0" && password[i] <= "9") //harus ada karakter dari 0-9 agar kondisi true dan loop berakhir
+            adaAngka = true; //deklarasi true mengakhiri loop
     }
-    
-    if (panjangCukup && adaAngka) {
+
+    if (panjangCukup && adaAngka) { //kondisi password diterima dan valid
         valid = true;
         console.log("Password valid!");
     } else {
         console.log("Password tidak valid! Harus minimal 6 karakter dan mengandung angka.");
     }
 
-} while (!valid);
+} while (!valid); // artinya NOT valid "Selama password BELUM valid, terus ulangi looping."
 console.log("Password berhasil dibuat: " + password);
---
+++ If/if3.js
@@ -0,0 +1,7 @@
+let angka = 7;
+// Tulis kode if-else statement di sini
+if (angka % 2 === 0) {
+    console.log(`${angka} adalah bilangan genap`)
+} else {
+    console.log(' adalah bilangan ganjil')
+}
