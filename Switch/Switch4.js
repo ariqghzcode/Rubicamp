2127fbbb927dca4598d6297ec54b25b6de7ddc69
ariq <ghazi.ariq18@gmail.com>
2026-08-06 11:54:43 +0700

    revisi switch4

--- Switch/Switch4.js
+++ Switch/Switch4.js
@@ -1,44 +1,38 @@
 let bulan = 1;
+let namaBulan = { 
+    1: "Januari",
+    2: "Februari",
+    3: "Maret",
+    4: "April",
+    5: "Mei",
+    6: "Juni",
+    7: "Juli",
+    8: "Agustus",
+    9: "September",
+    10: "Oktober",
+    11: "November",
+    12: "Desember",
+}
 // Tulis kode switch statement di sini
 switch (bulan) {
-    case 1:
-        Namabulan = "Januari"
-        jumlahhari = 31
-        break;
     case 2:
-        console.log("Bulan ke-" + bulan + " (Februari) memiliki 28 Hari")
-        break;
-    case 3:
-        console.log("Bulan ke-" + bulan + " (Maret) memiliki 31 Hari")
+        console.log(`Bulan ke-${bulan} (${namaBulan[bulan]}) memiliki 28 Hari`)
         break;
     case 4:
-        console.log("Bulan ke-" + bulan + " (April) memiliki 30 Hari")
-        break;
-    case 5:
-        console.log("Bulan ke-" + bulan + " (Mei) memiliki 31 Hari")
-        break;
     case 6:
-        console.log("Bulan ke-" + bulan + " (Juni) memiliki 30 Hari")
-        break;
-    case 7:
-        console.log("Bulan ke-" + bulan + " (Juli) memiliki 31 Hari")
-        break;
-    case 8:
-        console.log("Bulan ke-" + bulan + " (Agustus) memiliki 31 Hari")
-        break;
     case 9:
-        console.log("Bulan ke-" + bulan + " (September) memiliki 30 Hari")
-        break;
-    case 10:
-        console.log("Bulan ke-" + bulan + " (Oktober) memiliki 31 Hari")
-        break;
     case 11:
-        console.log("Bulan ke-" + bulan + " (November) memiliki 30 Hari")
+        console.log(`Bulan ke-${bulan} (${namaBulan[bulan]}) memiliki 30 Hari`)
         break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
     case 12:
-        console.log("Bulan ke-" + bulan + " (December) memiliki 31 Hari")
+        console.log(`Bulan ke-${bulan} (${namaBulan[bulan]}) memiliki 31 Hari`)
         break;
     default:
         break;
 }
-console.log("Bulan ke-" + bulan + " " + Namabulan + " memiliki " + jumlahhari + " Hari")
